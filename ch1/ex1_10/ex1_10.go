24bb957014a7f5851712048f42a9a431203436d9
Changjie Lin <lin061360@gmail.com>
2016-08-31 11:10:57 +0800

    Complete Exercise 1.10

---
+++ ch1/ex1_10/ex1_10.go
@@ -0,0 +1,52 @@
+/*
+- The reported time does not change much when running fetchall twice in succession.
+- Yes, I get the same content each time.
+*/
+
+// Fetchall fetches URLs in parallel and reports their times and sizes.
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	out, err := os.OpenFile("out.txt", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
+	if err != nil {
+		fmt.Println("Error at Openning file", err)
+		return
+	}
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch) // start a goroutine
+	}
+	for range os.Args[1:] {
+		io.WriteString(out, <-ch) // receive form channel ch
+	}
+	io.WriteString(
+		out, fmt.Sprintf("%.2fs elapsed\r\n", time.Since(start).Seconds()))
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err) // send to channel ch
+		return
+	}
+
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close() // don't leak resources
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs  %7d  %s", secs, nbytes, url)
+}
